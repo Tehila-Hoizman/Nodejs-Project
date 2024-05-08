8af1cc31ed222f963c0423db37304ff6e6f8a1d5
Your Nametehila <tehila0601@gmail.com>
2024-05-09 00:13:00 +0300

    fix token extracting

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -4,20 +4,22 @@ const jwt = require('jsonwebtoken');
 exports.auth = (req, res, next) => {
     try {
         const { authorization } = req.headers;//extracting the token from the header
-        const [, token] = authorization.splite(' ');
+        const [, token] = authorization.split(' ');
         const privateKey = process.env.JWT_SECRET || 'JWT_SECRET'; // secret string by which the token was created
 
         const data = jwt.verify(token, privateKey);// the data that the token contains
         req.user = data;//adding an data to the request
         next(); // moving to Route/Middlewar
     } catch (err) {
+        console.log("error: "+err );
         next({ message: err, status: 401 });
     }
 }
 exports.authAdmin = (req, res, next) => {
     try {
         const { authorization } = req.headers;//extracting the token from the header
-        const [, token] = authorization.splite(' ');
+        console.log(authorization);
+        const [, token] = authorization.split(' ');
         const privateKey = process.env.JWT_SECRET || 'JWT_SECRET'; // secret string by which the token was created
 
         const data = jwt.verify(token, privateKey);// the data that the token contains
@@ -27,6 +29,7 @@ exports.authAdmin = (req, res, next) => {
         else
             next({ message: 'only admin can get all user', status: 403 });
     } catch (err) {
+        console.log("error: "+err );
         next({ message: err, status: 401 });
     }
 }
