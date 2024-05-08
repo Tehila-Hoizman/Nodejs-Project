128d02a209940b158f7c2d80ccbd97bc885b8829
Your Nametehila <tehila0601@gmail.com>
2024-05-08 17:27:36 +0300

    add recipe controller and getRecipeByUserName  function

--- controllers/recipe.controller.js
+++ controllers/recipe.controller.js
@@ -17,17 +17,27 @@ exports.getAllRecipes = async (req, res, next) => {
         next(error);
     }
 }
-
 exports.getRecipeById = (req, res, next) => {
     const id = req.params.id;
-    if(!mongoose.Types.ObjectId.isValid(id))
-        next({message:'id is not valid'})
+    if (!mongoose.Types.ObjectId.isValid(id))
+        next({ message: 'id is not valid' })
     else
-        Recipe.findById(id,{ __v: false})
-    .then(r=>{
-        res.json(r);
-    })
-    .catch(err=>{
-        next({ message: 'recipe not found', status: 404 })
-    })
+        Recipe.findById(id, { __v: false })
+            .then(r => {
+                res.json(r);
+            })
+            .catch(err => {
+                next({ message: 'recipe not found', status: 404 })
+            })
 }
+exports.getRecipeByUserName = async (req, res, next) => {
+    const name = req.params.name;
+
+    try {
+        const recipes = await Recipe.find({ name: new RegExp(name) })
+            .select('-__v');
+        return res.json(recipes);
+    } catch (error) {
+        next(error);
+    }
+};
