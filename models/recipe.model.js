e855b9d5672befa116bf18d4a65b7cb6a0382186
Your Nametehila <tehila0601@gmail.com>
2024-05-08 17:13:10 +0300

    add recipe model

---
+++ models/recipe.model.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const layerSchema = new mongoose.Schema({
+    description:{type:String},
+    components:{type:[String]}
+})
+const recipeSchema = new mongoose.Schema({
+    name:{type:String,required:true},
+    categories:{type:[String]},
+    preparationTimeInMinute:{type:Number,default:10},
+    level:{type:Number,default:1,enum:[1,2,3,4,5]},
+    addDate:{type:Date,default:Date.now},
+    layers:{
+        type:[layerSchema],
+        validate:{
+            validator(v) {
+                return v && v.length <= 1;
+            },
+            message: 'must have most 1 layer'
+        }
+    },
+    preparation:{type:[String]},
+    image:{type:String},
+    isDisplay:{type:Boolean},
+    user:{type:String}
+})
+module.exports.Recipe = mongoose.model('recipes',recipeSchema)
